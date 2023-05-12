607b1ad902b281985347b21be81510e844ede508
victorgarciadss <garciavictor.1308@gmail.com>
2023-05-12 14:02:24 -0300

    feat: special query and hmologation ready

--- src/main/java/com/games/gameslist/repositories/GameRepository.java
+++ src/main/java/com/games/gameslist/repositories/GameRepository.java
@@ -1,9 +1,22 @@
 package com.games.gameslist.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.games.gameslist.entities.Game;
+import com.games.gameslist.projections.GameMinProjection;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
     
+    @Query(nativeQuery = true, value = """
+		SELECT games.id, games.title, games.game_year AS gameYear, games.img_url AS imgUrl,
+		games.short_description AS shortDescription, belonging.position
+		FROM games
+		INNER JOIN belonging ON games.id = belonging.game_id
+		WHERE belonging.list_id = :listId
+		ORDER BY belonging.position
+			""")
+    List<GameMinProjection> searchByList(Long listId);
 }
